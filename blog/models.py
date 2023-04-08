7efd02e8500873ec70b0b4a7f56307c14f4c72f5
rkyzk <reikoyazaki@yahoo.com>
2023-04-08 12:32:28 +0000

    make Post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,71 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+STATUS = ((0, "Draft"), (1, "Submitted"), (2, "Published"), (3, "Declined"))
+
+REGION = (('N/A', 'N/A'),
+          ('NAM', 'North America'),
+          ('CAM', 'Central America'),
+          ('CRB', 'Caribbean'),
+          ('SAM', 'South America'),
+          ('NEU', 'Northern Europe'),
+          ('WEU', 'Western Europe'),
+          ('EEU', 'Eastern Europe'),
+          ('SEU', 'Southern Europe'),
+          ('NAF', 'North Africa'),
+          ('WAF', 'Western Africa'),
+          ('MAF', 'Middle Africa'),
+          ('EAF', 'Eastern Africa'),
+          ('SAF', 'Southern Africa'),
+          ('WAS', 'Western Asia'),
+          ('CAS', 'Central Asia'),
+          ('EAS', 'Eastern Asia'),
+          ('SAS', 'Southern Asia'),
+          ('SAS', 'Southeastern Asia'),
+          ('ANZ', 'Australia and New Zealand'),
+          ('PIS', 'Pacific Islands'))
+
+CATEGORY = (('animals', 'Protecting animals'),
+            ('aquatic system', 'Protecting the aquatic system'),
+            ('saving soil & trees', 'Protecting soil & trees'),
+            ('saving resources', 'Saving resources'),
+            ('eco-conscious diet', 'Eco-conscious diet'),
+            ('others', 'Others'))
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=80, unique=True)
+    slug = models.SlugField(max_length=80, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name="posts")
+    featured_flag = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    published_on = models.DateTimeField(null=True, blank=True)
+    content = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder', blank=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='post_likes', blank=True)
+    region = models.CharField(max_length=30, choices=REGION, default='N/A')
+    category = models.CharField(max_length=30, choices=CATEGORY,
+                                default='Others')
+    bookmark = models.ManyToManyField(User, related_name='bookmarked',
+                                      blank=True)
+
+
+    class Meta:
+        ordering = ['-created_on']
+
+
+    def __str__(self):
+        return self.title
+
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+    def pub_date(self):
+        return self.published_on.strftime("%m/%d/%Y")
