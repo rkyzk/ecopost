5d5e9b840dae8bb5bc61a6320b6b175af902e82a
rkyzk <reikoyazaki@yahoo.com>
2023-04-08 19:16:05 +0000

    write post method for add story view to save/submit the new article

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
@@ -59,6 +60,12 @@ class Post(models.Model):
         ordering = ['-created_on']
 
 
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+        
     def __str__(self):
         return self.title
 
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic, View
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
 from .forms import PostForm, PhotoForm
 from .models import Post
 
@@ -12,8 +13,9 @@ class PostList(generic.ListView):
 
 
 class AddStory(LoginRequiredMixin, View):
-    """Create a new post """
+    
     def get(self, request, *args, **kwargs):
+        """Post an empty form for writing a new post """
         return render(
             request,
             "add_story.html",
@@ -22,3 +24,31 @@ class AddStory(LoginRequiredMixin, View):
                 "photo_form": PhotoForm()
             }
         )
+
+    
+    def post(self, request, *args, **kwargs):
+        """Save or submit a new post"""
+        post_form = PostForm(self.request.POST)
+        photo_form = PhotoForm(self.request.POST, self.request.FILES)
+        if post_form.is_valid() and photo_form.is_valid():
+            post_form.instance.author = self.request.user
+            photo = photo_form.save(commit=False)
+            post_form.instance.featured_image = photo.image
+            if 'submit' in self.request.POST.keys():
+                post_form.instance.status = 1
+                post_form.save()
+                messages.add_message(self.request, messages.SUCCESS, 'Your draft has been submitted.')
+            else:
+                post_form.save()
+                messages.add_message(self.request, messages.SUCCESS, 'Your draft has been saved.')    
+        else:
+            print("error occured")
+        return render(
+            request,
+            "add_story.html",
+            {
+                "post_form": PostForm(),
+                "photo_form": PhotoForm()
+            }
+        )
+
