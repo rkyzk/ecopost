d7359b4dbd419f296e232e4718edbf3121e9c644
rkyzk <reikoyazaki@yahoo.com>
2023-04-15 01:52:19 +0000

    write tests for PostForm in test_forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -10,9 +10,9 @@ class PostForm(forms.ModelForm):
         fields = ['title', 'content', 'featured_image', 'region', 'category']
 
 
-        def __init__(self, *args, **kwargs):
-            super(PostForm, self).__init__(*args, **kwargs)
-            self.fields['featured_image'].required = False
+        # def __init__(self, *args, **kwargs):
+        #     super(PostForm, self).__init__(*args, **kwargs)
+        #     self.fields['featured_image'].required = False
 
 
 class CommentForm(forms.ModelForm):
@@ -20,18 +20,3 @@ class CommentForm(forms.ModelForm):
         model = Comment
         fields = ('body',)
 
-
-# class PhotoForm(forms.ModelForm):
-
-#     class Meta:
-#         model = PostForm
-#         fields = ('image',)
-
-#     featured_image = CloudinaryFileField(
-#         options = {
-#             'tags': "directly_uploaded",
-#             'crop': 'fill_pad', 'width': 510, 'height': 340,
-#             'gravity': 'auto',
-#             'q_auto': 'good'
-#         })
-   
--- blog/test_forms.py
+++ blog/test_forms.py
@@ -1,23 +1,98 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from .forms import PostForm
+from .forms import PostForm, CommentForm
+from .models import Post, Comment
 
 
-# class TestPostForm(TestCase):
+class TestPostForm(TestCase):
 
-    # def test_Post_title_is_required(self):
-    #     """Test if the title is required for 'Post' model."""
-    #     user = User.objects.create(username='test',
+    def setUp(self):  # done before each test is done
+        """create test users and posts"""
+        self.user_1 = User.objects.create(username="test1",
+                                          password="password")
+        self.user_2 = User.objects.create(username="test2",
+                                          password="password")
+        self.post1 = Post.objects.create(title="title_1",
+                                         author=self.user_1,
+                                         content="test sentences")
+        self.post2 = Post.objects.create(title="title_2",
+                                         author=self.user_2,
+                                         content="test 2 sentences")
 
-    #                                password='password')
-    #     form = PostForm({
-    #         'title': '',
-    #         'author': user,
-    #         'content': 'content'
-    #     })
-    #     self.assertFalse(form.is_valid())
-    #     self.assertIn('title', form.error.keys())
-    #     self.assertEqual(form.errors['title'][0], 'This field is required.')
+
+    def test_post_title_is_required(self):
+        form = PostForm({
+            'title': '',
+            'author': self.user_1,
+            'content': 'content',
+            'region': 'N/A',
+            'category': 'others'
+        })
+        self.assertFalse(form.is_valid())
+        self.assertIn('title', form.errors.keys())
+        self.assertEqual(form.errors['title'][0], 'This field is required.')
+
+    
+    def test_post_content_is_required(self):
+        form = PostForm({
+            'title': 'title_3',
+            'author': self.user_1,
+            'content': '',
+            'region': 'N/A',
+            'category': 'others'
+        })
+        self.assertFalse(form.is_valid())
+        self.assertIn('content', form.errors.keys())
+        self.assertEqual(form.errors['content'][0],
+                         'This field is required.')
+
+
+    def test_post_featured_image_is_not_required(self):
+        form = PostForm({
+            'title': 'title_3',
+            'author': self.user_2,
+            'content': 'content',
+            'region': 'N/A',
+            'category': 'others'
+        })
+        self.assertTrue(form.is_valid())
+
+
+    def test_post_region_is_required(self):
+        form = PostForm({
+            'title': 'title_3',
+            'author': self.user_2,
+            'content': 'content',
+            'region': '',
+            'category': 'others'
+        })
+        self.assertFalse(form.is_valid())
+        self.assertIn('region', form.errors.keys())
+        self.assertEqual(form.errors['region'][0],
+                         'This field is required.')
+
+
+    def test_post_category_is_required(self):
+        form = PostForm({
+            'title': 'title_3',
+            'author': self.user_2,
+            'content': 'content',
+            'region': 'N/A',
+            'category': ''
+        })
+        self.assertFalse(form.is_valid())
+        self.assertIn('category', form.errors.keys())
+        self.assertEqual(form.errors['category'][0],
+                         'This field is required.')
+
+
+    def test_fields_are_explicit_in_form_metaclass(self):
+        form = PostForm()
+        self.assertEqual(
+            form.Meta.fields,
+            ['title', 'content', 'featured_image', 'region', 'category']
+        )
+
 
 if __name__ == "__main__":
     main()
