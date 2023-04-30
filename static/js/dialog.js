156727c3bba51c6fddbf8c3c2389c3e6b551c1e0
rkyzk <reikoyazaki@yahoo.com>
2023-04-30 02:50:54 +0000

    add css to let the pop up menu appear over the pre-existing html element

--- static/js/dialog.js
+++ static/js/dialog.js
@@ -1,18 +1,18 @@
 // Confirm before deleting the post
 const DELETE_POST = "Are you sure you want to delete your post?  You won't be able to retrieve the draft."
 $('.delete-post').click(function() {
-return confirm(DELETE_POST);
+  return confirm(DELETE_POST);
 });
 
 // Confirm before deleting comment
 const DELETE_COMMENT = "Are you sure you want to delete your comment?"
 $('.delete-comment').click(function() {
-return confirm(DELETE_COMMENT);
+  return confirm(DELETE_COMMENT);
 })
 
 // Confirm before submitting posts
-const SUBMIT_POST = "After submiiting your post, you won't be able to" + \
+const SUBMIT_POST = "After submiiting your post, you won't be able to" +
                     "update or delete it.  Would you like to proceed?"
 $('.submit-post').click(function() {
-return confirm(SUBMIT_POST);
+  return confirm(SUBMIT_POST);
 })
