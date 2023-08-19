49b7d5e6b1e752c74c1af6411410af8583725f04
rkyzk <reikoyazaki@yahoo.com>
2023-08-19 04:56:13 +0000

    update requirements.txt

--- ecopost/settings.py
+++ ecopost/settings.py
@@ -29,13 +29,13 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = [
     'ecopost.herokuapp.com',
-    '8000-rkyzk-ecopost-s3szjwrmkw1.ws-us104.gitpod.io'
+    '8000-rkyzk-ecopost-cpavqrbd9y1.ws-us104.gitpod.io'
 ]
 
 # Application definition
